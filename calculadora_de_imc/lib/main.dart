87c4395923aa43e25aa24dc52bf685a1963ac91f
LuizD13 <luizclaudio_azevedo@hotmail.com>
2019-12-18 17:50:21 -0300

    IMCs do projeto

--- calculadora_de_imc/lib/main.dart
+++ calculadora_de_imc/lib/main.dart
@@ -27,6 +27,16 @@ class _HomeState extends State<Home> {
       debugPrint('$imc');
       if(imc < 18.6){
         _infotext = "Abaixo do peso (${imc.toStringAsPrecision(3)})";
+      }else if(imc >=18.6 && imc < 24.9){
+        _infotext = "Peso ideal (${imc.toStringAsPrecision(3)})";
+      }else if(imc >=24.9 && imc < 29.9){
+        _infotext = "Levemente acima do peso (${imc.toStringAsPrecision(3)})";
+      }else if(imc >=29.9 && imc < 34.9){
+        _infotext = "Obesidade Grau I (${imc.toStringAsPrecision(3)})";
+      }else if(imc >=34.9 && imc < 39.9){
+        _infotext = "Obesidade Grau II (${imc.toStringAsPrecision(3)})";
+      }else if(imc >=40){
+        _infotext = "Obesidade Grau III (${imc.toStringAsPrecision(3)})";
       }
 
     });
