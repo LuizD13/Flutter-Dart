677b27acb32387ca6f2b4d0d23abd21c11487b3e
LuizD13 <luizclaudio_azevedo@hotmail.com>
2019-12-19 19:37:54 -0300

    Campos e configuração do conversor de moedas

--- conversor_de_moedas/lib/main.dart
+++ conversor_de_moedas/lib/main.dart
@@ -1,6 +1,124 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
+const request = 'https://api.hgbrasil.com/finance?format=json&key=7a7770c3';
+
+void main() async {
+  runApp(MaterialApp(
+      home: Home(),
+      theme: ThemeData(
+        hintColor: Colors.amber,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.amber))),
+      )));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  final realController = TextEditingController();
+  final dolarController = TextEditingController();
+  final euroController = TextEditingController();
+
+  double dolar;
+  double euro;
+
+  void _realchanged(String Text){
+
+  }
+
+  void _dolarchanged(String Text){
+
+  }
+
+  void _eurochanged(String Text){
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+            title: Text("\$ Conversor \$"),
+            backgroundColor: Colors.amber,
+            centerTitle: true),
+        body: FutureBuilder<Map>(
+          future: getData(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
+                  child: Text(
+                    'Carregando Dados...',
+                    style: TextStyle(color: Colors.amber, fontSize: 25),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              default:
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text(
+                      'Erro ao carregar dados.',
+                      style: TextStyle(color: Colors.amber, fontSize: 25),
+                      textAlign: TextAlign.center,
+                    ),
+                  );
+                } else {
+                  dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                  euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+
+                  return SingleChildScrollView(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      children: <Widget>[
+                        Icon(
+                          Icons.monetization_on,
+                          size: 150,
+                          color: Colors.amber,
+                        ),
+                        Divider(),
+                        buildTextField("Real", "R\$",realController,_realchanged),
+                        Divider(),
+                        buildTextField("Dólar", "US\$",dolarController,_dolarchanged),
+                        Divider(),
+                        buildTextField("Euro", "€",euroController,_eurochanged),
+                      ],
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                    ),
+                  );
+                }
+            }
+          },
+        ));
+  }
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
+
+Widget buildTextField(String label, String prefix, TextEditingController c,Function f) {
+  return TextField(
+    decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.amber),
+        border: OutlineInputBorder(),
+        prefixText: prefix),
+    style: TextStyle(color: Colors.amber, fontSize: 25
+    ),
+    onChanged: f,
+    keyboardType: TextInputType.number,
+  );
 }
