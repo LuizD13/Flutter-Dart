e649eba3a5275ade35e183222c80d4b622b27be5
LuizD13 <luizclaudio_azevedo@hotmail.com>
2019-12-20 11:23:48 -0300

    Finalização do conversor de moeda

--- conversor_de_moedas/lib/main.dart
+++ conversor_de_moedas/lib/main.dart
@@ -24,7 +24,6 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   final realController = TextEditingController();
   final dolarController = TextEditingController();
   final euroController = TextEditingController();
@@ -32,16 +31,42 @@ class _HomeState extends State<Home> {
   double dolar;
   double euro;
 
-  void _realchanged(String Text){
-
+  void _clearAll() {
+    realController.text = "";
+    dolarController.text = "";
+    euroController.text = "";
   }
 
-  void _dolarchanged(String Text){
+  void _realchanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
 
+    double real = double.parse(text);
+    dolarController.text = (real / dolar).toStringAsFixed(2);
+    euroController.text = (real / euro).toStringAsFixed(2);
   }
 
-  void _eurochanged(String Text){
+  void _dolarchanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double dolar = double.parse(text);
+    realController.text = (dolar * this.dolar).toStringAsFixed(2);
+    euroController.text = (dolar * this.dolar / euro).toStringAsFixed(2);
+  }
 
+  void _eurochanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+
+    double euro = double.parse(text);
+    realController.text = (euro * this.euro).toStringAsFixed(2);
+    dolarController.text = (euro * this.euro / dolar).toStringAsFixed(2);
   }
 
   @override
@@ -88,11 +113,14 @@ class _HomeState extends State<Home> {
                           color: Colors.amber,
                         ),
                         Divider(),
-                        buildTextField("Real", "R\$",realController,_realchanged),
+                        buildTextField(
+                            "Real", "R\$", realController, _realchanged),
                         Divider(),
-                        buildTextField("Dólar", "US\$",dolarController,_dolarchanged),
+                        buildTextField(
+                            "Dólar", "US\$", dolarController, _dolarchanged),
                         Divider(),
-                        buildTextField("Euro", "€",euroController,_eurochanged),
+                        buildTextField(
+                            "Euro", "€", euroController, _eurochanged),
                       ],
                       crossAxisAlignment: CrossAxisAlignment.stretch,
                     ),
@@ -109,15 +137,16 @@ Future<Map> getData() async {
   return json.decode(response.body);
 }
 
-Widget buildTextField(String label, String prefix, TextEditingController c,Function f) {
+Widget buildTextField(
+    String label, String prefix, TextEditingController c, Function f) {
   return TextField(
+    controller: c,
     decoration: InputDecoration(
         labelText: label,
         labelStyle: TextStyle(color: Colors.amber),
         border: OutlineInputBorder(),
         prefixText: prefix),
-    style: TextStyle(color: Colors.amber, fontSize: 25
-    ),
+    style: TextStyle(color: Colors.amber, fontSize: 25),
     onChanged: f,
     keyboardType: TextInputType.number,
   );
