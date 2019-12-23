c36b60d9d360977b745781b6a1e81799badf72ee
LuizD13 <luizclaudio_azevedo@hotmail.com>
2019-12-23 16:50:18 -0300

    Buscador de GIFs (Em produção)

---
+++ buscador_de_gifs/lib/main.dart
@@ -0,0 +1,14 @@
+import "package:flutter/material.dart";
+import "package:buscador_de_gifs/ui/home_page.dart";
+
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(
+      inputDecorationTheme: InputDecorationTheme(
+          enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white))
+      )
+    ),
+  ));
+}
+
