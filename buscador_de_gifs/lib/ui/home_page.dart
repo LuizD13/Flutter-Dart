a7670c76d3b53bb8e5d4d33fd1485e60b76b2f83
LuizD13 <luizclaudio_azevedo@hotmail.com>
2019-12-26 19:01:08 -0300

    Novo form para buscador de gifs

--- buscador_de_gifs/lib/ui/home_page.dart
+++ buscador_de_gifs/lib/ui/home_page.dart
@@ -20,7 +20,7 @@ class _HomePageState extends State<HomePage> {
           "https://api.giphy.com/v1/gifs/trending?api_key=TIg9RAeC0aE5VWIM3nBzbHyDQsUgX2OY&limit=20&rating=G");
     } else if (_search != null) {
       response = await http.get(
-          "https://api.giphy.com/v1/gifs/search?api_key=TIg9RAeC0aE5VWIM3nBzbHyDQsUgX2OY&q$_search=&limit=20&offset=$_offset&rating=G&lang=pt");
+          "https://api.giphy.com/v1/gifs/search?api_key=TIg9RAeC0aE5VWIM3nBzbHyDQsUgX2OY&q=$_search=&limit=19&offset=$_offset&rating=G&lang=pt");
     }
 
     return json.decode(response.body);
@@ -53,6 +53,12 @@ class _HomePageState extends State<HomePage> {
                   border: OutlineInputBorder()),
               style: TextStyle(color: Colors.white, fontSize: 18),
               textAlign: TextAlign.center,
+              onSubmitted: (text){
+                setState(() {
+                  _search = text;
+                  _offset = 0;
+                });
+              },
             ),
           ),
           Expanded(
@@ -72,8 +78,10 @@ class _HomePageState extends State<HomePage> {
                           strokeWidth: 5.0,
                         ));
                   default:
-                    if (snapshot.hasError) return Container();
-                    else return _createGifTable(context, snapshot);
+                    if (snapshot.hasError)
+                      return Container();
+                    else
+                      return _createGifTable(context, snapshot);
                 }
               },
             ),
@@ -82,7 +90,50 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot){
 
+  int _getCount(List data){
+    if (_search == null){
+      return data.length;
+
+    }else{
+      return data.length + 1;
+    }
+  }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+        padding: EdgeInsets.all(10.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
+        itemCount: _getCount(snapshot.data["data"]),
+        itemBuilder: (context, index) {
+          if(_search == null || index < snapshot.data["data"].length){
+          return GestureDetector(
+            child: Image.network(
+              snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+              height: 300.0,
+              fit: BoxFit.cover,
+            ),
+          );}
+          else{
+            return Container(
+              child: GestureDetector(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.add,color:Colors.white,size: 70.0,),
+                    Text("Carregar mais...",
+                      style: TextStyle(color:Colors.white, fontSize: 22.0),)
+                  ],
+                ),
+            onTap: (){
+                  setState(() {
+                    _offset += 19;
+                  });
+            },
+              )
+          );
+          }
+        });
   }
 }
